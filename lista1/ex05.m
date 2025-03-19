a10b9a310082f75e97d361ee754796d33bbe13c7
GustavoSMC1 <gustavosilvamarquesdecastro@gmail.com>
2025-03-19 20:15:45 -0300

    Create ex05.m

---
+++ lista1/ex05.m
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+int numero; // declarcao da variavel para armazenar o numero
+
+// solicita ao usuario para digitar um numero inteiro 
+
+cout << "digite um numero inteiro";
+cin >> numero; // le o numero digitado
+
+// exibe o numero na tela
+
+cout << "o numero digitadofoi:" << numero << endl;
+
+return 0;
+	
+}
