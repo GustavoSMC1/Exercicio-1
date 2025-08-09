59fe97376f21eb8f2eb66820532136d0e2e5d75c
GustavoSMC1 <gustavosilvamarquesdecastro@gmail.com>
2025-08-09 20:46:58 -0300

    Create Exe1.cpp

---
+++ Exercicios Daniel - Estrutura Sequencial /Exe1.cpp
@@ -0,0 +1,17 @@
+    #include <iostream>
+    using namespace std;
+    
+    int main()
+    {
+        float nota1,nota2,media;
+        cout << " Digite a primeira nota" << endl;
+        cin >> nota1;
+        
+        cout << " Digite a segunda nota" << endl;
+        cin >> nota2;
+        
+        media = ( nota1 + nota2 ) /2;
+        cout << " A media do aluno é " << media << endl;
+    
+        return 0;
+    }
