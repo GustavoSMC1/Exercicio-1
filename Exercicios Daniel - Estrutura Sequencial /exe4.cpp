89149f0dae93f98b11d60547f5c05e19a056a1c8
GustavoSMC1 <gustavosilvamarquesdecastro@gmail.com>
2025-08-09 21:08:33 -0300

    Create exe4.cpp

---
+++ Exercicios Daniel - Estrutura Sequencial /exe4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   double num1,num2,resultado;
+        cout << " Digite dois numeros : ";
+        cin >> num1 >> num2;
+            resultado = (num1*num2);
+            cout << "O resultado é : " << resultado;
+    return 0;
+}
