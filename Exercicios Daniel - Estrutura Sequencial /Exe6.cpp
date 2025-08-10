42e5db2679c4081c6b32c8a9decf4b03851df0b6
GustavoSMC1 <gustavosilvamarquesdecastro@gmail.com>
2025-08-09 21:31:49 -0300

    Create Exe6.cpp

---
+++ Exercicios Daniel - Estrutura Sequencial /Exe6.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    double Celsius,fahrenheit;
+        cout << " Digite a temperatura em graus Celsius :";
+        cin >> Celsius;
+            fahrenheit = (Celsius * 9/5)+32;
+                 
+                cout << Celsius << " graus Celsius equivalem a " << fahrenheit << " graus Fahrenheit." << endl;
+    return 0;
