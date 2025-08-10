4b60046f0a653d945d12f0103406d1dcdadac933
GustavoSMC1 <gustavosilvamarquesdecastro@gmail.com>
2025-08-10 17:48:38 -0300

    Create Exer1.cpp

---
+++ Exercicios Daniel Python/Exer1.cpp
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a nota do 1º bimestre: "))
+nota2 = float(input("Digite a nota do 2º bimestre: "))
+
+media = (nota1 + nota2) / 2
+
+print(f"A média do aluno é: {media:.2f}")
